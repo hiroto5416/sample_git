ec9b074108a29b4dc7ff78d678b67eccd265ba39
hiroto5416 <hiroto5416@icloud.com>
2022-09-15 20:30:04 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,14 +4,14 @@ module SessionsHelper
     def log_in(user)
       session[:user_id] = user.id
     end
-  
+
     # ユーザーのセッションを永続的にする
     def remember(user)
       user.remember
       cookies.permanent.encrypted[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
     end
-  
+
     # 記憶トークンcookieに対応するユーザーを返す
     def current_user
       if (user_id = session[:user_id])
@@ -24,12 +24,17 @@ module SessionsHelper
         end
       end
     end
-  
+
+    # 渡されたユーザーがカレントユーザーであればtrueを返す
+    def current_user?(user)
+      user && user == current_user
+    end
+
     # ユーザーがログインしていればtrue、その他ならfalseを返す
     def logged_in?
       !current_user.nil?
     end
-  
+
     # 現在のユーザーをログアウトする
     def log_out
       session.delete(:user_id)
@@ -49,4 +54,15 @@ module SessionsHelper
       session.delete(:user_id)
       @current_user = nil
     end
-  end
+
+    # 記憶したURL（もしくはデフォルト値）にリダイレクト
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+
+    # アクセスしようとしたURLを覚えておく
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+      end
+    end
